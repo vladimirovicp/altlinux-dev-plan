97d33bfa562fecb9068779729aca2f8661779c7f
John Doe <johndoe@example.com>
2025-07-25 17:50:39 +0400

    feat: add course c 3_2

---
+++ lab/course_c/day_3_3/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    int count = 100;
+
+    for (size_t i = 0; i < count; i++)
+    {
+        printf("%d", i);
+    }
+    
+
+    return 0;
+}
