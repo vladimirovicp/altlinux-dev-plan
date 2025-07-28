72d1f6e9511a1839ff5e147af1f11b2559159b0e
John Doe <johndoe@example.com>
2025-07-28 18:18:50 +0400

    feat: lab assci

---
+++ lab/course_c/day_4_1/build/CMakeFiles/start.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/start.dir/link.d"
+  "CMakeFiles/start.dir/main.cpp.o"
+  "CMakeFiles/start.dir/main.cpp.o.d"
+  "start"
+  "start.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/start.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab/course_c/day_4_1/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+
+    // case '\n': printf("%4d - \\n\n",i);
+
+    /*
+    for(int i=0; i<56; i++){
+        switch (i)
+        {
+        case '\0': printf("%4d - Null\n",i);
+        break;
+        case '\n': printf("%4d - Enter\n",i);
+        break;
+        case '\t': printf("%4d - tab\n",i);
+        break;
+        case '\a': printf("%4d - sound\n",i);
+        break;
+        
+        default: printf("%4d - %c\n",i,i);
+            break;
+        }
+        
+    }
+        
+*/
+
+    return 0;
+}
